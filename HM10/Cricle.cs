e6250839ab47d99e8620c2c8afd8b20ccce2b883
Vadim <multikvadimka1@mail.ru>
2019-04-21 16:07:52 +0300

    READY

---
+++ HM10/Cricle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM10
+{
+   public class Cricle : IBase
+    {
+        public string name { get; set; }
+        public int side { get; set; }
+        public object Ar()
+        {
+            Random random = new Random();
+            side = random.Next(1, 10);
+            double area = Math.PI * side;
+            return area;
+        }      
+    }
+}
