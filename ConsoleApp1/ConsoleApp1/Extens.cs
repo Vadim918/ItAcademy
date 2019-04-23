b5827646ca0123243c1acce71c5a834b7b2e835d
user <user@ECHTP-50-15>
2019-04-23 20:41:57 +0300

    Ready

---
+++ ConsoleApp1/ConsoleApp1/Extens.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class StringExtension
+    {
+        public static void WordCount(this System.Int32 del,string str)
+        {
+            str.Substring(str.Length, str.Length - 5);
+            
+        }
+    }
+}
+}
